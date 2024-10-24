a5af7a2b77d49e01cd3d644fef15130ac70d0ee7
eldahl <71466904+eldahl@users.noreply.github.com>
2024-10-24 20:01:41 +0200

    Initial upload

---
+++ Demo/Program.cs
@@ -0,0 +1,16 @@
+﻿// See https://aka.ms/new-console-template for more information
+
+using FeatureHubSDK;
+
+FeatureLogging.DebugLogger += (sender, s) => Console.WriteLine("DEBUG: " + s); 
+FeatureLogging.TraceLogger += (sender, s) => Console.WriteLine("TRACE: " + s); 
+FeatureLogging.InfoLogger += (sender, s) => Console.WriteLine("INFO: " + s); 
+FeatureLogging.ErrorLogger += (sender, s) => Console.WriteLine("ERROR: " + s);
+
+var config = new EdgeFeatureHubConfig("http://featurehub:8085", "<Configure you SDK key here>");
+
+await Task.Run(() =>
+{
+    Console.WriteLine("Hello World");
+});
+
